57489618d144042890c9f0c7c42a5d569873c6ca
saeed <saeed_j51@yahoo.com>
2020-05-17 04:07:40 -0400

    Compiling Assets(mix): add: public>css>vendor> fonts styles modify: webpack.mix.js npm run dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .styles(['public/css/vendor/styles.css',
+        'public/css/vendor/fonts.css'], 'public/css/all.css');
