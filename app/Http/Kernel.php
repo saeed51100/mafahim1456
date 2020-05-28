1d202a70749dde3864d9284ff5902a20164c7690
saeed <saeed_j51@yahoo.com>
2020-05-28 10:24:44 -0400

    users-authentication 5-3 php artisan make:middleware AlwaysRedirect

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\AlwaysRedirect;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -20,6 +21,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+
+        \App\Http\Middleware\AlwaysRedirect::class
     ];
 
     /**
