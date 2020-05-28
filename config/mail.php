f8334691d13ec1215990dfad1e21ce8386495ba2
saeed <saeed_j51@yahoo.com>
2020-05-28 08:08:47 -0400

    users-authentication 4-6

--- config/mail.php
+++ config/mail.php
@@ -83,8 +83,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('no-reply@test.com', 'hello@example.com'),
+        'name' => env('Saeed', 'Example'),
     ],
 
     /*
