4aaf263bbd0c5c1f528600d933b78a25362c2b10
saeed <saeed_j51@yahoo.com>
2020-06-04 13:47:52 -0400

    justify sidebar items to right

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -41,7 +41,7 @@
 
 <div class="container-fluid">
     <div class="row">
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light text-right sidebar collapse">
             <div class="sidebar-sticky pt-3">
                 <ul class="nav flex-column">
                     <li class="nav-item">
